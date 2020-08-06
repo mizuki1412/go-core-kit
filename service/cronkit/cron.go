96e7015ce2294fe512f221378e477f66189234dd
yingchaojie <yingchaojie@163.com>
2020-08-06 19:38:27 +0800

    update cronkit

--- service/cronkit/cron.go
+++ service/cronkit/cron.go
@@ -1,8 +1,11 @@
 package cronkit
 
 import (
+	"github.com/mizuki1412/go-core-kit/class/exception"
 	"github.com/mizuki1412/go-core-kit/service/configkit"
+	"github.com/mizuki1412/go-core-kit/service/logkit"
 	"github.com/robfig/cron/v3"
+	"github.com/spf13/cast"
 )
 
 var scheduler *cron.Cron
@@ -27,5 +30,28 @@ func RemovePool(key string) {
 	if ok {
 		v.Stop()
 		delete(pool, key)
+	}
+}
+
+// 给默认的scheduler add func， 封装上recover
+func AddFunc(spec string, fun func()) {
+	_, err := Scheduler().AddFunc(spec, func() {
+		defer func() {
+			if err := recover(); err != nil {
+				var msg string
+				if e, ok := err.(exception.Exception); ok {
+					//msg = e.Msg
+					// 带代码位置信息
+					logkit.Error(e.Error())
+				} else {
+					msg = cast.ToString(err)
+					logkit.Error(msg)
+				}
+			}
+		}()
+		fun()
+	})
+	if err != nil {
+		panic(exception.New(err.Error()))
 	}
 }
