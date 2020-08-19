3ba094c71b527f1e3a02d55716a03553aaa33c01
yingchaojie <yingchaojie@163.com>
2020-08-19 23:21:09 +0800

    update

--- library/mathkit/math.go
+++ library/mathkit/math.go
@@ -1,5 +1,7 @@
 package mathkit
 
+import "github.com/shopspring/decimal"
+
 func GroupNum(sum int, group int) int {
 	if sum == 0 {
 		return 1
@@ -9,4 +11,10 @@ func GroupNum(sum int, group int) int {
 	} else {
 		return sum/group + 1
 	}
+}
+
+// 保留小数
+func FloatRound(val float64, num int32) float64 {
+	f, _ := decimal.NewFromFloat(val).Round(num).Float64()
+	return f
 }
