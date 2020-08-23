bb96e3100528c1fd46c5e2688c905bfde3fdb5f3
yingchaojie <yingchaojie@163.com>
2020-08-23 15:53:45 +0800

    update

--- class/exception/Exception.go
+++ class/exception/Exception.go
@@ -19,9 +19,9 @@ func New(msg string, skip1 ...int) Exception {
 		skip = skip1[0]
 	}
 	_, file, line, _ := runtime.Caller(skip)
-	stack := make([]string, 0, 3)
+	stack := make([]string, 0, 4)
 	stack = append(stack, getStackInfo(file, line))
-	for i := 1; i < 3; i++ {
+	for i := 1; i < 4; i++ {
 		_, file1, line1, ok := runtime.Caller(skip + i)
 		if ok {
 			stack = append(stack, getStackInfo(file1, line1))
--- library/mathkit/math.go
+++ library/mathkit/math.go
@@ -1,6 +1,9 @@
 package mathkit
 
-import "github.com/shopspring/decimal"
+import (
+	"github.com/shopspring/decimal"
+	"math"
+)
 
 func GroupNum(sum int, group int) int {
 	if sum == 0 {
@@ -15,6 +18,9 @@ func GroupNum(sum int, group int) int {
 
 // 保留小数
 func FloatRound(val float64, num int32) float64 {
+	if math.IsNaN(val) {
+		return 0
+	}
 	f, _ := decimal.NewFromFloat(val).Round(num).Float64()
 	return f
 }
