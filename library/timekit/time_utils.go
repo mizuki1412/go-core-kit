a6c491d68525502ffdf34a8378237e0c98de7d6d
yingchaojie <yingchaojie@163.com>
2021-01-05 00:45:51 +0800

    update

---
+++ library/timekit/time_utils.go
@@ -0,0 +1,48 @@
+package timekit
+
+import "time"
+
+// 取base中不含layout的部分
+func GetNotOverlapArray(base []time.Time, layout []time.Time) ([]time.Time, []time.Time) {
+	base1 := base[0].Unix()
+	base2 := base[1].Unix()
+	layout1 := layout[0].Unix()
+	layout2 := layout[1].Unix()
+	if layout1 >= base2 || layout2 <= base1 {
+		return base, nil
+	}
+	if layout2 < base2 && layout1 < base1 {
+		return []time.Time{layout[1], base[1]}, nil
+	} else if layout1 > base1 && layout2 > base2 {
+		return []time.Time{base[0], layout[0]}, nil
+	} else if layout1 > base1 && layout2 < base2 {
+		return []time.Time{base[0], layout[0]}, []time.Time{layout[1], base[1]}
+	} else {
+		return nil, nil
+	}
+}
+
+// 合并两组时间，返回nil则未合并
+func MergeArray(base []time.Time, layout []time.Time) []time.Time {
+	base1 := base[0].Unix()
+	base2 := base[1].Unix()
+	layout1 := layout[0].Unix()
+	layout2 := layout[1].Unix()
+	if layout1 >= base2 || layout2 <= base1 {
+		return nil
+	}
+	if layout2 < base2 && layout1 < base1 {
+		return []time.Time{layout[0], base[1]}
+	} else if layout1 > base1 && layout2 > base2 {
+		return []time.Time{base[0], layout[1]}
+	} else if layout1 > base1 && layout2 < base2 {
+		return base
+	} else {
+		return layout
+	}
+}
+
+func GetNotOverlapSegment(base []time.Time, layouts [][]time.Time) [][]time.Time {
+	// todo
+	return nil
+}
--- service/sqlkit/pghelper/pg_helper.go
+++ service/sqlkit/pghelper/pg_helper.go
@@ -8,7 +8,7 @@ import (
 // 生成sql中: sth in (select unnest(Array[?,?,?])) []interface{}
 // arr必须不能空
 // 注意使用时 args...
-func genUnnestString(arr []string) (string, []interface{}) {
+func GenUnnestString(arr []string) (string, []interface{}) {
 	flags := make([]string, len(arr))
 	args := make([]interface{}, len(arr))
 	for i := 0; i < len(arr); i++ {
@@ -18,7 +18,7 @@ func genUnnestString(arr []string) (string, []interface{}) {
 	return " (select unnest(Array[" + strings.Join(flags, ", ") + "]))", args
 }
 
-func genUnnestInt(arr []int32) (string, []interface{}) {
+func GenUnnestInt(arr []int32) (string, []interface{}) {
 	flags := make([]string, len(arr))
 	args := make([]interface{}, len(arr))
 	for i := 0; i < len(arr); i++ {
@@ -28,7 +28,7 @@ func genUnnestInt(arr []int32) (string, []interface{}) {
 	return " (select unnest(Array[" + strings.Join(flags, ", ") + "]::int[]))", args
 }
 
-func genUnnestInt64(arr []int64) (string, []interface{}) {
+func GenUnnestInt64(arr []int64) (string, []interface{}) {
 	flags := make([]string, len(arr))
 	args := make([]interface{}, len(arr))
 	for i := 0; i < len(arr); i++ {
@@ -39,7 +39,7 @@ func genUnnestInt64(arr []int64) (string, []interface{}) {
 }
 
 // 返回 Array[?,?,?]
-func genArrayFlagString(arr []string) (string, []interface{}) {
+func GenArrayFlagString(arr []string) (string, []interface{}) {
 	flags := make([]string, len(arr))
 	args := make([]interface{}, len(arr))
 	for i := 0; i < len(arr); i++ {
@@ -49,7 +49,7 @@ func genArrayFlagString(arr []string) (string, []interface{}) {
 	return " Array[" + strings.Join(flags, ", ") + "]::varchar[]", args
 }
 
-func genArrayFlagInt(arr []int32) (string, []interface{}) {
+func GenArrayFlagInt(arr []int32) (string, []interface{}) {
 	flags := make([]string, len(arr))
 	args := make([]interface{}, len(arr))
 	for i := 0; i < len(arr); i++ {
@@ -61,52 +61,52 @@ func genArrayFlagInt(arr []int32) (string, []interface{}) {
 
 // 封装到builder
 func WhereUnnestString(builder squirrel.SelectBuilder, sqlPrefix string, arr []string) squirrel.SelectBuilder {
-	flag, arg := genUnnestString(arr)
+	flag, arg := GenUnnestString(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereUnnestInt(builder squirrel.SelectBuilder, sqlPrefix string, arr []int32) squirrel.SelectBuilder {
-	flag, arg := genUnnestInt(arr)
+	flag, arg := GenUnnestInt(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereUnnestInt64(builder squirrel.SelectBuilder, sqlPrefix string, arr []int64) squirrel.SelectBuilder {
-	flag, arg := genUnnestInt64(arr)
+	flag, arg := GenUnnestInt64(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereArrayString(builder squirrel.SelectBuilder, sqlPrefix string, arr []string) squirrel.SelectBuilder {
-	flag, arg := genArrayFlagString(arr)
+	flag, arg := GenArrayFlagString(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereArrayInt(builder squirrel.SelectBuilder, sqlPrefix string, arr []int32) squirrel.SelectBuilder {
-	flag, arg := genArrayFlagInt(arr)
+	flag, arg := GenArrayFlagInt(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 // 封装到builder
 func WhereUnnestStringU(builder squirrel.UpdateBuilder, sqlPrefix string, arr []string) squirrel.UpdateBuilder {
-	flag, arg := genUnnestString(arr)
+	flag, arg := GenUnnestString(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereUnnestIntU(builder squirrel.UpdateBuilder, sqlPrefix string, arr []int32) squirrel.UpdateBuilder {
-	flag, arg := genUnnestInt(arr)
+	flag, arg := GenUnnestInt(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereUnnestInt64U(builder squirrel.UpdateBuilder, sqlPrefix string, arr []int64) squirrel.UpdateBuilder {
-	flag, arg := genUnnestInt64(arr)
+	flag, arg := GenUnnestInt64(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereArrayStringU(builder squirrel.UpdateBuilder, sqlPrefix string, arr []string) squirrel.UpdateBuilder {
-	flag, arg := genArrayFlagString(arr)
+	flag, arg := GenArrayFlagString(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
 
 func WhereArrayIntU(builder squirrel.UpdateBuilder, sqlPrefix string, arr []int32) squirrel.UpdateBuilder {
-	flag, arg := genArrayFlagInt(arr)
+	flag, arg := GenArrayFlagInt(arr)
 	return builder.Where(sqlPrefix+flag, arg...)
 }
