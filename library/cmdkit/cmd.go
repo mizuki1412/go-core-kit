aa9d64ae49b00517d4706c3d6aa76569044dae36
yingchaojie <yingchaojie@163.com>
2021-02-17 21:04:27 +0800

    update cmdkit

--- library/cmdkit/cmd.go
+++ library/cmdkit/cmd.go
@@ -1,10 +1,92 @@
 package cmdkit
 
 import (
+	"bufio"
+	"errors"
 	"github.com/mizuki1412/go-core-kit/service/logkit"
+	"github.com/spf13/cast"
+	"io"
+	"io/ioutil"
 	"os/exec"
 	"path/filepath"
+	"time"
 )
+
+type RunParams struct {
+	Timeout int  `comment:"超时时间s"`
+	Async   bool `comment:"异步处理返回值"`
+}
+
+func Run(command string, params ...RunParams) (string, error) {
+	// todo 判断系统环境
+	var param RunParams
+	if len(params) == 0 {
+		param = RunParams{}
+	} else {
+		param = params[0]
+	}
+
+	cmd := exec.Command("/bin/sh", "-c", command)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+
+	if err = cmd.Start(); err != nil {
+		return "", err
+	}
+	if !param.Async {
+		if param.Timeout > 0 {
+			to := make(chan map[string]interface{})
+			go func() {
+				ret0, err2 := getRet(stdout, stderr, cmd)
+				to <- map[string]interface{}{"ret": ret0, "err": err2}
+			}()
+			select {
+			case <-time.After(time.Duration(param.Timeout) * time.Second):
+				return "", errors.New("timeout")
+			case m := <-to:
+				ret := m["ret"].(string)
+				var err error
+				if m["err"] != nil {
+					err = m["err"].(error)
+				}
+				return cast.ToString(ret), err
+			}
+		} else {
+			ret, err := getRet(stdout, stderr, cmd)
+			return ret, err
+		}
+	}
+	return "", nil
+}
+
+func getRet(stdout io.ReadCloser, stderr io.ReadCloser, cmd *exec.Cmd) (string, error) {
+	ret := ""
+	reader := bufio.NewReader(stdout)
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		ret += line
+	}
+	bytesErr, err := ioutil.ReadAll(stderr)
+	if err != nil {
+		return ret, err
+	}
+	if len(bytesErr) != 0 {
+		return ret, errors.New(string(bytesErr))
+	}
+	if err = cmd.Wait(); err != nil {
+		return ret, err
+	}
+	return ret, nil
+}
 
 func LinuxCmd(name string, args ...string) error {
 	command := exec.Command(name, args...)
