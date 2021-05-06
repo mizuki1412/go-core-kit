75b48f62d143c7a2b0a98e4330124add51937e58
yingchaojie <yingchaojie@163.com>
2021-05-06 15:12:15 +0800

    update

---
+++ class/lock_wait.go
@@ -0,0 +1,31 @@
+package class
+
+import "sync"
+
+// 用于异步等待
+
+type LockWait struct {
+	sync.RWMutex
+	Data map[string]chan map[string]interface{}
+}
+
+func (th *LockWait) Open(cid string) {
+	th.Lock()
+	defer th.Unlock()
+	th.Data[cid] = make(chan map[string]interface{})
+}
+
+func (th *LockWait) Close(cid string) {
+	th.Lock()
+	defer th.Unlock()
+	delete(th.Data, cid)
+}
+
+func (th *LockWait) ChanExist(cid string) bool {
+	_, ok := th.Data[cid]
+	return ok
+}
+
+func (th *LockWait) Chan(cid string) chan map[string]interface{} {
+	return th.Data[cid]
+}
