2161cce5c1a425deaebc21818e8b2b1a22925f57
yingchaojie <yingchaojie@163.com>
2021-08-02 23:03:43 +0800

    update xml

--- class/string.go
+++ class/string.go
@@ -2,15 +2,48 @@ package class
 
 import (
 	"database/sql/driver"
+	"encoding/xml"
 	"github.com/mizuki1412/go-core-kit/class/utils"
 	"github.com/mizuki1412/go-core-kit/library/jsonkit"
 	"github.com/spf13/cast"
 )
 
-// 同时继承scan和value方法
+// String 同时继承scan和value方法
 type String struct {
 	String string
 	Valid  bool
+}
+
+func (th String) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	// todo ?
+	if th.Valid {
+		return e.EncodeElement(th.String, start)
+	}
+	return e.EncodeElement(nil, start)
+}
+func (th *String) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var value string
+	// Read tag content into value
+	err := d.DecodeElement(&value, &start)
+	if err != nil {
+		return err
+	}
+	if value == "" {
+		th.Valid = false
+	} else {
+		th.Valid = true
+		th.String = utils.UnquoteIfQuoted([]byte(value))
+	}
+	return nil
+}
+func (th *String) UnmarshalXMLAttr(attr xml.Attr) error {
+	if attr.Value == "" {
+		th.Valid = false
+	} else {
+		th.Valid = true
+		th.String = utils.UnquoteIfQuoted([]byte(attr.Value))
+	}
+	return nil
 }
 
 func (th String) MarshalJSON() ([]byte, error) {
