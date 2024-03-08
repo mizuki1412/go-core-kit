6bc8e4bd27f911ee38324f7fe30c9e39f7452253
ycj <yingchaojie@163.com>
2024-03-08 16:01:18 +0800

    v2.0.5: fix time cost

--- library/timekit/timecost/time_cost.go
+++ library/timekit/timecost/time_cost.go
@@ -2,35 +2,41 @@ package timecost
 
 import (
 	"fmt"
-	"github.com/mizuki1412/go-core-kit/v2/library/timekit"
 	"github.com/mizuki1412/go-core-kit/v2/service/logkit"
 	"time"
 )
 
 // TimeCost 程序执行耗时记录打点
 type TimeCost struct {
+	Title string
 	Start time.Time
+	//Unit string
 }
 
 type Params struct {
-	showTitleMsg bool
+	//Unit string // 时间显示单位 s,m
 }
 
 func NewTimeCost(title string, params ...Params) TimeCost {
 	t := TimeCost{
+		Title: title,
 		Start: time.Now(),
 	}
-	param := Params{}
-	if len(params) > 0 {
-		param = params[0]
-	}
-	if param.showTitleMsg {
-		logkit.Debug(title + ": " + t.Start.Format(timekit.TimeLayout))
-	}
+	//param := Params{}
+	//if len(params) > 0 {
+	//	param = params[0]
+	//}
+	//t.Unit = param.Unit
 	return t
 }
 
-func (th *TimeCost) PrintCost(msg string) {
-	logkit.Debug(fmt.Sprintf("%s: %dms", msg, time.Since(th.Start).Milliseconds()))
+// 每次打印后重新计时
+func (th *TimeCost) PrintCost(msg string, logInfo ...bool) {
+	message := fmt.Sprintf("time-cost【%s|%s】%fs", th.Title, msg, time.Since(th.Start).Seconds())
+	if len(logInfo) > 0 && logInfo[0] {
+		logkit.Info(message)
+	} else {
+		logkit.Debug(message)
+	}
 	th.Start = time.Now()
 }
