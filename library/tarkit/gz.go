8bcae0bc1dc7cfa22cb9f2652364b9087b49464b
ycj <yingchaojie@163.com>
2024-03-10 22:35:50 +0800

    v2.0.5: add gzip

---
+++ library/tarkit/gz.go
@@ -0,0 +1,31 @@
+package tarkit
+
+import (
+	"bytes"
+	"compress/gzip"
+	"github.com/mizuki1412/go-core-kit/v2/class/exception"
+	"io"
+)
+
+// GZipBytes 压缩
+func GZipBytes(data []byte) []byte {
+	var input bytes.Buffer
+	g := gzip.NewWriter(&input)
+	defer g.Close()
+	_, err := g.Write(data)
+	if err != nil {
+		panic(exception.New(err.Error()))
+	}
+	g.Flush()
+	return input.Bytes()
+}
+
+// UGZipBytes 解压
+func UGZipBytes(data []byte) []byte {
+	var in bytes.Buffer
+	in.Write(data)
+	r, _ := gzip.NewReader(&in)
+	defer r.Close()
+	undatas, _ := io.ReadAll(r)
+	return undatas
+}
