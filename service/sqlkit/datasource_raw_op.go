4f21665ef03fee1f3636a529b1c918861876add9
ycj-zchy <yingchaojie@163.com>
2025-02-21 22:11:30 +0800

    v2.1.2: add formatRaw

--- service/sqlkit/datasource_raw_op.go
+++ service/sqlkit/datasource_raw_op.go
@@ -2,12 +2,27 @@ package sqlkit
 
 import (
 	"github.com/mizuki1412/go-core-kit/v2/class/exception"
+	"github.com/mizuki1412/go-core-kit/v2/library/timekit"
 	"github.com/spf13/cast"
+	"time"
 )
 
 /**
 * 用于简单场景的sql，无规定model
  */
+
+func (ds *DataSource) QueryOne(sql string, args ...any) any {
+	rows := ds.Query(sql, args)
+	defer rows.Close()
+	for rows.Next() {
+		ret, err := rows.SliceScan()
+		if err != nil {
+			panic(exception.New(err.Error()))
+		}
+		return ret[0]
+	}
+	return 0
+}
 
 func (ds *DataSource) QueryOneNumber(sql string, args ...any) int64 {
 	rows := ds.Query(sql, args)
@@ -76,4 +91,40 @@ func (ds *DataSource) QueryListString(sql string, args ...any) []string {
 		list = append(list, cast.ToString(ret[0]))
 	}
 	return list
+}
+
+// FormatRawMap 原始查出的map[string]any 转为 map[string]string 用于sql语句中
+// 注意防注入
+func (ds *DataSource) FormatRawMap(rows map[string]any) map[string]string {
+	res := make(map[string]string)
+	for key, val := range rows {
+		res[key] = ds.FormatRawValue(val)
+	}
+	return res
+}
+
+func (ds *DataSource) FormatRawValue(val any) string {
+	if val == nil {
+		return "null"
+	}
+	// todo 其他情况
+	switch val.(type) {
+	case string:
+		return "'" + val.(string) + "'"
+	case []uint8:
+		return "'" + string(val.([]uint8)) + "'"
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return cast.ToString(val)
+	case float32, float64:
+		return cast.ToString(val)
+	case bool:
+		return cast.ToString(val)
+	case time.Time:
+		t := val.(time.Time)
+		if t.IsZero() {
+			return "null"
+		}
+		return "'" + t.In(timekit.GetLocation()).Format(timekit.TimeLayout) + "'"
+	}
+	return "'" + cast.ToString(val) + "'"
 }
